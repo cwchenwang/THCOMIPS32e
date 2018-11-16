ed106693bad5156e07a4349fa43fe910fd894809
Daisy_Zhang <zhang_daichi@163.com>
2018-11-16 21:43:31 +0800

    Regfile module

---
+++ src/Regfile.v
@@ -0,0 +1,23 @@
+`include "defines.v"
+
+module Regfile(
+    input wire rst,
+    input wire clk,
+
+    // write
+    input wire we,
+    input wire[`RegAddrBus] waddr,
+    input wire[`RegBus] wdata,
+
+    // read reg1
+    input wire re1,
+    input wire[`RegAddrBus] raddr1,
+    output reg[`RegBus] rdata1,
+
+    //read reg2
+    input wire re2,
+    input wire[`RegAddrBus] raddr2,
+    output reg[`RegBus] rdata2,
+);
+
+endmodule
