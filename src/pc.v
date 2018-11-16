a3c36336ee0f049f656e87e06a74b38738bd3597
Daisy_Zhang <zhang_daichi@163.com>
2018-11-16 21:25:20 +0800

    PC module

---
+++ src/pc.v
@@ -0,0 +1,15 @@
+`include "defines.v"
+
+module pc(
+    input wire clk,
+    input wire rst,
+    input wire flush,
+    input wire stall,
+    input wire[`InstAddrBus] new_pc,
+    input wire branch_flag_i,
+    input wire[`RegAddrBus] branch_target_address_i,
+    output reg[`InstAddrBus] pc,
+    output reg ce
+)
+
+endmodule
