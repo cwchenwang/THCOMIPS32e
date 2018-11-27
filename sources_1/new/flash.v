d1afdaf8af6660e738df66654615ef10a5a3942d
chwangthu <chwangthu@gmail.com>
2018-11-27 22:51:19 +0800

    Modify some bugs in flash

--- sources_1/new/flash.v
+++ sources_1/new/flash.v
@@ -1,83 +1,82 @@
-`include "defines.v"
+`include "defines.vh"
 
 module flash(
-    input wire rst,
-    input wire clk,
-    output wire flash_ce,
-    output wire flash_we,
-    output wire flash_oe,
-    output wire flash_rp,
-    output wire flash_byte,
-    output wire flash_vpen,
-
-    input wire[22:1] addr,
-    output wire[15:0] data_out,
-    output wire[22:1] flash_addr,
-    inout wire[15:0] flash_data
+    input wire rst, clk,
+    input wire[22:0] addr,
+    inout wire[15:0] flash_data,
+    
+    output reg flash_ce,
+    output reg flash_we,
+    output reg flash_oe,
+    output wire flash_rp,   //0ÖØÖÃ£¬1¹¤×÷
+    output wire flash_byte, //0×Ö½Ú·½Ê½£¬1×Ö·½Ê½
+    output wire flash_vpen, //Ğ´±£»¤£¬³£ÖÃ1
+    output reg[22:0] flash_addr,
+    output reg[15:0] data_out
 );
 
-    // åº”è¯¥ç”±ç½‘ç«™å¹³å°å®ç°
+    // Ó¦¸ÃÓÉÍøÕ¾Æ½Ì¨ÊµÏÖ
     /*reg[15:0] data_flash[0:4194303];
     initial $readmemh ("flash.data", data_flash);
     assign flash_data = { data_flash[addr][7:0], data_flash[addr][15:8] };*/
 
     assign flash_rp = 1'b1;
-    assign flash_ce = 1'b0;
+//    assign flash_ce = 1'b0;
     assign flash_vpen = 1'b1;
     assign flash_byte = 1'b1;
 
     reg[2:0] cur_state = 3'b000;
-
+    reg[15:0] flash_data_tep;
+    
+    assign flash_data = flash_data_tep;
+    
     always @(posedge clk or posedge rst) begin
-		if (reset) begin
+		if (rst) begin
+		    flash_ce <= 1'b1;
 			flash_oe <= 1'b1;
 			flash_we <= 1'b1;
 			cur_state <= 3'b000;
-			flash_data <= 16'bz;
+			flash_data_tep <= 16'bz;
             //ctl_read_last <= ctl_read;
         end
 		else begin
 			case(cur_state)
 				3'b000:begin
+				    flash_ce <= 1'b0;   //Æ¬Ñ¡
                     flash_we <= 1'b0;
                     cur_state <= 3'b001;
                     //if (ctl_read /= ctl_read_last) then
                     //ctl_read_last <= ctl_read;
 				end
-
-                3'b001 begin
-					flash_data <= 4'h00FF;
+                3'b001:begin
+					flash_data_tep <= 4'h00FF;
 					cur_state <= 3'b010;
                 end
-
-                3'b010 begin
+                3'b010:begin
 					flash_we <= 1'b1;
 					cur_state <= 3'b011;
                 end
-
-                3'b011 begin
+                3'b011:begin
 					flash_oe <= 1'b0;
 					flash_addr <= addr;
-					flash_data <= 16'bz;
+					flash_data_tep <= 16'bz;
 					cur_state <= 3'b100;
                 end
-
-                3'b100 begin
-					data_out <= flash_data;
-					state <= 3'b101;        // å³æ‰§è¡Œdefault
+                3'b100:begin
+					data_out <= flash_data_tep;
+					cur_state <= 3'b101;        // ¼´Ö´ĞĞdefault
                 end
-					
-				default begin
+				3'b101:begin
 					flash_oe <= 1'b1;
 					flash_we <= 1'b1;
-					flash_data <= 16'bz;
-					state <= 3'b000;
+					flash_data_tep <= 16'bz;
+					cur_state <= 3'b000;
                 end
-
+                default:begin
+                    cur_state <= 3'b000;
+                end
 			endcase
-
 		end
-
 	end
 
 endmodule
