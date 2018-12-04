b145f0e3d55f2308c2ff70969ee52785f1ed4c51
Daisy_Zhang <zhang_daichi@163.com>
2018-12-04 22:47:43 +0800

    game finished with a simple map

--- vga_game/vga_box_game.v
+++ vga_game/vga_box_game.v
@@ -40,6 +40,8 @@ reg[10:0] people_center_v;
 reg[10:0] people_center_h;
 reg[10:0] box_center_v;
 reg[10:0] box_center_h;
+reg[10:0] target_center_v;
+reg[10:0] target_center_h;
 
 // init
 initial begin
@@ -52,61 +54,84 @@ initial begin
     people_center_h <= 11'b0 + 100;
     box_center_v <= 11'b0 + 300;
     box_center_h <= 11'b0 + 165;
+    target_center_v <= 11'b0 + 500;
+    target_center_h <= 11'b0 + 620;
     //addr <= 19'b0;
 end
-
-/*assign red = (((hdata >= 375 && hdata <= 425) && ((vdata >= 225 && vdata <= 275) || (vdata >= 325 && vdata <= 375))) || ((hdata >= 595 && hdata <= 605) && (vdata >= 100 && vdata <= 500)) || ((hdata >= 195 && hdata <= 205) && (vdata >= 100 && vdata <= 500)) || (((hdata >= 45 && hdata <= 55) || (hdata >= 745 && hdata <= 755)) && (vdata >= 100 && vdata <= 500)) || (((vdata >= 95 && vdata <= 105) || (vdata >= 295 && vdata <= 305) || (vdata >= 495 && vdata <= 505)) && ((hdata >= 50 && hdata <= 200) ||(hdata >= 600 && hdata <= 750)))) ? 3'b111 : 0;
-assign green = 0;
-assign blue = 0;*/
 
 assign red = red_temp;
 assign green = green_temp;
 assign blue = blue_temp;
 
 always @ (*) begin
-    //if(rst_btn) begin
-        red_temp <= ((vdata == people_center_v && hdata >= people_center_h - 25 && hdata <= people_center_h + 25) || (hdata == people_center_h && vdata >= people_center_v - 25 && vdata <= people_center_v + 25) || (vdata == people_center_v + 25 && hdata >= people_center_h - 15 && hdata <= people_center_h + 15) || (hdata == people_center_h - 15 && vdata >= people_center_v + 25 && vdata <= people_center_v + 50) || (hdata == people_center_h + 15 && vdata >= people_center_v + 25 && vdata <= people_center_v + 50) || (vdata >= people_center_v - 50 && vdata <= people_center_v - 25 && hdata >= people_center_h - 10 && hdata <= people_center_h + 10)) || (vdata >= box_center_v - 50 && vdata <= box_center_v + 50 && hdata <= box_center_h + 40 && hdata >= box_center_h - 40) ? 3'b111 : 0;
-        blue_temp <= 3'b000;
-        green_temp <= 3'b000;
-    //end
+    red_temp <= (((vdata == people_center_v && hdata >= people_center_h - 25 && hdata <= people_center_h + 25) || (hdata == people_center_h && vdata >= people_center_v - 25 && vdata <= people_center_v + 25) || (vdata == people_center_v + 25 && hdata >= people_center_h - 15 && hdata <= people_center_h + 15) || (hdata == people_center_h - 15 && vdata >= people_center_v + 25 && vdata <= people_center_v + 50) || (hdata == people_center_h + 15 && vdata >= people_center_v + 25 && vdata <= people_center_v + 50) || (vdata >= people_center_v - 50 && vdata <= people_center_v - 25 && hdata >= people_center_h - 10 && hdata <= people_center_h + 10)) || (vdata >= box_center_v - 50 && vdata <= box_center_v + 50 && hdata <= box_center_h + 40 && hdata >= box_center_h - 40)) ? 3'b111 : 0;
+    blue_temp <= (hdata >= target_center_h - 40 && hdata <= target_center_h + 40 && vdata >= target_center_v - 50 && vdata <= target_center_v + 50 ) ? 3'b011:0;
+    green_temp <= (vdata >= 350 && hdata >= 530 && hdata <= 580) || (vdata <= 350 && hdata >= 205 && hdata <= 255) ? 3'b011:0;
 end
 
-always @ (posedge clk_btn) begin
+always @ (posedge clk_btn or posedge rst_btn) begin
+    if(rst_btn) begin
+        people_center_v <= 11'b0 + 300;
+        people_center_h <= 11'b0 + 100;
+        box_center_v <= 11'b0 + 300;
+        box_center_h <= 11'b0 + 165;
+    end else begin
     if(direction == 4'b1000) begin          // up
-        people_center_v <= people_center_v - 100;
-        box_center_v <= box_center_v - 100;
+        if(people_center_h == box_center_h && box_center_v == people_center_v - 100 && (box_center_v - 50 - 100 < 75 || (box_center_v - 350 == 50 && people_center_h == 230))) begin
+        
+        end else if(people_center_v - 350 <= 50 && people_center_h >= 205 && people_center_h <= 255) begin
+        
+        end else if(people_center_v - 50 - 100 >= 75) begin
+            people_center_v <= people_center_v - 100;
+            if(box_center_v == people_center_v - 100 && people_center_h == box_center_h) begin
+                if(box_center_v - 50 - 100 >= 75 && 350 - box_center_v >= 50) begin
+                    box_center_v <= box_center_v - 100;
+                end
+            end
+        end
     end else if(direction == 4'b0100) begin  //down
-        people_center_v <= people_center_v + 100;
-        box_center_v <= box_center_v + 100;
+        if(people_center_h == box_center_h && box_center_v == people_center_v + 100 && (box_center_v + 50 + 100 > 599 || (350 - box_center_v == 50 && people_center_h == 555))) begin
+        
+        end else if(350 - people_center_v <= 50 && people_center_h >= 530 && people_center_h <= 580) begin
+        
+        end else if(people_center_v + 50+ 100 <= 599) begin
+            people_center_v <= people_center_v + 100;
+            if(box_center_v == people_center_v + 100 && people_center_h == box_center_h) begin
+                if(box_center_v + 50 + 100 <= 599 && box_center_v - 350 >= 50) begin
+                    box_center_v <= box_center_v + 100;
+                end
+            end
+        end
     end else if(direction == 4'b0010) begin  //left
-        people_center_h <= people_center_h - 65;
-        box_center_h <= box_center_h - 65;
+        if(people_center_v == box_center_v &&  box_center_h == people_center_h - 65 && (box_center_h - 37 - 65 < 20 || (box_center_h - 580 == 40 && box_center_v > 350) || (box_center_h - 255 == 40 && box_center_v < 350))) begin
+        
+        end else if((people_center_h - 580 <= 40 && people_center_v >= 350) || (people_center_h - 255 <= 40 && people_center_v <= 350)) begin
+        
+        end else if(people_center_h - 25 - 65 >= 20) begin
+            people_center_h <= people_center_h - 65;
+            if(box_center_h == people_center_h - 65 && people_center_v == box_center_v) begin
+                if(box_center_h - 37 - 65 >= 20 && box_center_h - 580 >= 40 && box_center_h - 255 >= 40) begin
+                    box_center_h <= box_center_h - 65;
+                end
+            end
+        end
     end else if(direction == 4'b0001) begin  //right
-        people_center_h <= people_center_h + 65;
-        box_center_h <= box_center_h + 65;
+        if(people_center_v == box_center_v && box_center_h == people_center_h + 65 && (box_center_h + 37 + 65 > 799 || (530 - box_center_h == 40 && box_center_v > 350) || (205 - box_center_h == 40 && box_center_v < 350))) begin
+        
+        end else if((530 - people_center_h <= 40 && people_center_v >= 350) || (205 - people_center_h <= 40 && people_center_v <= 350)) begin
+        
+        end else if(people_center_h + 25 + 65 <= 799) begin
+            people_center_h <= people_center_h + 65;
+            if(box_center_h == people_center_h + 65 && people_center_v == box_center_v) begin
+                if(box_center_h + 37 + 65 <= 799 && 530 - box_center_h >= 40 && 205 - box_center_h >= 40) begin
+                    box_center_h <= box_center_h + 65;
+                end
+            end
+        end
+    end
     end
   
 end
-
-//always @ (posedge right_move) begin
-//    people_center_h <= people_center_h + 65;
-//    box_center_h <= box_center_h + 65;
-//end
-
-//always @ (posedge left_move) begin
-//    people_center_h <= people_center_h - 65;
-//    box_center_h <= box_center_h - 65;
-//end
-
-//always @ (posedge down_move) begin
-//    people_center_v <= people_center_v + 100;
-//    box_center_v <= box_center_v + 100;
-//end
-
-//always @ (posedge up_move) begin
-//    people_center_v <= people_center_v - 100;
-//    box_center_v <= box_center_v - 100;
-//end
 
 // hdata
 always @ (posedge clk)
@@ -128,17 +153,6 @@ begin
             vdata <= vdata + 1;
     end
 end
-
-// 待添加由地址或寄存器得到数据
-/*always @ (posedge clk) begin
-    if (hdata == 0 & vdata == 0) begin
-        addr <= 19'b0; 
-    end else begin
-        if (data_enable) begin
-            addr <= addr + 1;
-        end
-    end
-end*/
 
 // hsync & vsync & blank
 assign hsync = ((hdata >= HFP) && (hdata < HSP)) ? HSPP : !HSPP;
